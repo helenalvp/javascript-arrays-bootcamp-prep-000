a57d16a154eb667f793516388e1633f4af66a423
Helena Liliana Vaquera <helenalvp@gmail.com>
2020-08-19 23:26:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,37 +1,37 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 var array=[];
 function addElementToBeginningOfArray(array,element){
-  [element, ...array ];
+  return [element, ...array ];
 }
 //ill have to come back to this
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  return array.unshift(element)
 }
 //ill have to come back to this one too
 function addElementToEndOfArray(array,element){
-  [...array, element];
+return  [...array, element];
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
+  return array.push(element);
 }
 //this did not come up at all for the thinks i want to do.
 function accessElementInArray(array,index) {
-  cosole.log(array[index]);
+  return cosole.log(array[index]);
 }
 
 //i feel like there is a problem with me telling the computer what is an array
 //what is an element and what is and index
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift();
+  return array.shift();
 }
 function removeElementFromBeginningOfArray(array) {
-  array.slice();
+  return array.slice();
 }
 //what
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop();
+  return array.pop();
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0,array.length-1);
+  return array.slice(0,array.length-1);
 }
